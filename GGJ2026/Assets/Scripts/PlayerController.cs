9454f6bf583b79e49ee8464fc91db352cbd92bf8
Tianzuo Peng <pengtianzuo@gmail.com>
2026-01-30 12:55:13 +0800

    更新滑动准心

--- GGJ2026/Assets/Scripts/PlayerController.cs
+++ GGJ2026/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,11 @@ public class PlayerController : MonoBehaviour
 {
     private Camera mainCamera;
     public float followSpeed = 10f;
+
+    public GameObject target;
+
+    public float minVerticalDistance = 1.2f;
+    public float followStrength = 1.0f;
 
     void Start()
     {
@@ -24,5 +30,26 @@ public class PlayerController : MonoBehaviour
             mouseWorldPos,
             followSpeed * Time.deltaTime
         );
+
+        target.transform.position = UpdateTargetY(mouseWorldPos.y, target.transform.position.y);
     }
+
+    Vector3 UpdateTargetY(float mouseY,float currentTargetY)
+    {
+        // 同步上下
+        float targetY = Mathf.Lerp(currentTargetY, mouseY, followStrength);
+
+        // 最小垂直距离约束
+        float deltaY = targetY - mouseY;
+
+        if (Mathf.Abs(deltaY) < minVerticalDistance)
+        {
+            float sign = Mathf.Sign(deltaY == 0 ? 1 : deltaY);
+            targetY = mouseY + sign * minVerticalDistance;
+        }
+
+        return new Vector3(0, targetY, 0);
+    }
+
+
 }
